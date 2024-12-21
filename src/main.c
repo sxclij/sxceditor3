ad7e00fd01d2a5c5cf14ce15c80dbb98a1c0d269
sxclij <sxclij@gmail.com>
2024-12-21 02:35:27 +0000

    rename pc to ip

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
+#include <stdint.h>
 #include <unistd.h>
 
+#define mem_size (1 << 16)
+
+enum mem_global {
+    mem_global_zero,
+    mem_global_ip,
+    mem_global_sp,
+    mem_global_bp,
+};
+
+struct global {
+    uint64_t mem[mem_size];
+};
+
+static struct global global;
+
 int main() {
-    write(STDOUT_FILENO, "Good morning world!\n", 21);
     return 0;
 }
